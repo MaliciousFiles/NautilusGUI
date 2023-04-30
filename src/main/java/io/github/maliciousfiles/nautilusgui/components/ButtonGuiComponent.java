771bcac054089cb6d5ad383680b5512c51d9f373
Malcolm Roalson <mtroalson@gmail.com>
2023-04-30 17:07:44 -0500

    add closeOnClick to buttons

--- src/main/java/io/github/maliciousfiles/nautilusgui/components/ButtonGuiComponent.java
+++ src/main/java/io/github/maliciousfiles/nautilusgui/components/ButtonGuiComponent.java
@@ -1,5 +1,6 @@
 package io.github.maliciousfiles.nautilusgui.components;
 
+import io.github.maliciousfiles.nautilusgui.page.GuiPage;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
 import java.util.function.Consumer;
@@ -7,14 +8,24 @@ import java.util.function.Consumer;
 public class ButtonGuiComponent extends ItemGuiComponent {
 
     private Consumer<InventoryClickEvent> action;
+    private boolean closeOnClick = false;
 
     public ButtonGuiComponent setAction(Consumer<InventoryClickEvent> action) {
         this.action = action;
         return this;
     }
 
+    public ButtonGuiComponent setCloseOnClick(boolean closeOnClick) {
+        this.closeOnClick = closeOnClick;
+        return this;
+    }
+
     @Override
     public void handleClick(InventoryClickEvent e) {
         action.accept(e);
+        if (closeOnClick && e.getInventory().getHolder() instanceof GuiPage page) {
+            InventoryClickEvent.getHandlerList().unregister(page.getGui());
+            e.getWhoClicked().closeInventory();
+        }
     }
 }
